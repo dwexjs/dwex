9a41e1bf6b1a7ee4c73ea682c547ce5b01a38504
mxvsh <xencodes@icloud.com>
2025-10-27 02:12:52 +0530

    docs: update icons and page structure

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -5,14 +5,6 @@ const withMDX = createMDX();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: '/docs',
-        destination: '/docs/getting-started/introduction',
-      },
-    ]
-  },
 };
 
 export default withMDX(config);
