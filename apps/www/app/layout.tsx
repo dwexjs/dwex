4bc3583c0f03780a62a858cc46d2cec35658f22b
mxvsh <xencodes@icloud.com>
2025-10-27 10:27:34 +0530

    feat: add analytics script to layout for enhanced tracking

--- apps/www/app/layout.tsx
+++ apps/www/app/layout.tsx
@@ -2,6 +2,7 @@ import { RootProvider } from "fumadocs-ui/provider/next";
 import type { Metadata } from "next";
 import "./global.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({
 	subsets: ["latin"],
@@ -15,6 +16,11 @@ export const metadata: Metadata = {
 export default function Layout({ children }: LayoutProps<"/">) {
 	return (
 		<html lang="en" className={inter.className} suppressHydrationWarning>
+			<Script
+				defer
+				src="https://analytics.monawwar.io/script.js"
+				data-website-id="5599c2d7-3249-40a2-accd-65ba865f986d"
+			/>
 			<body className="flex flex-col min-h-screen">
 				<RootProvider>{children}</RootProvider>
 			</body>
